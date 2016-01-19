488a8df10e0d7a47f5df5cec17a8fa37b8cbc81e
liangjiapei1103 <liang98@purdue.edu>
2016-01-18 22:41:10 -0500

    npm install mongodb --save

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var seert = require('assert');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -30,6 +32,13 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
 });
 
 // error handlers
